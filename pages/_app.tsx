18b5c4cc765bc057f437b9b0a68c3ef98a142b34
Charlie85270 <rabiller.charlie@gmail.com>
2023-12-17 20:28:57 -0500

    improve loader

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -97,7 +97,18 @@ const App: FC<AppProps> = ({ Component, pageProps }: AppProps) => {
   }, [router.events]);
 
   if (isLoading || !theme) {
-    return <p>Loading</p>;
+    return (
+      <div className="flex w-full items-center justify-center mt-44">
+        <div
+          className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] text-primary motion-reduce:animate-[spin_1.5s_linear_infinite]"
+          role="status"
+        >
+          <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+            Loading...
+          </span>
+        </div>
+      </div>
+    );
   }
   return (
     <div id="root">
